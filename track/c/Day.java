f43b5570aaa23d60c5c05c4535bb79d83da7de64
Mukkara Tejasree <mukkaratejasree@gmail.com>
2026-08-04 12:11:48 +0530

    day3changes

---
+++ track/c/Day.java
@@ -0,0 +1,11 @@
+package track.c;
+public class Day {
+    public static void main(String[] args){
+        System.out.println("Monday");
+        System.out.print("Tuesday");
+        System.out.println("Wednesday");
+        System.out.print("friday");
+        System.out.println("Saturday");
+        System.out.println("Sunday");
+    }
+}
