885b4fd33832572098068c51ce82d24ad540f1bb
Mukkara Tejasree <mukkaratejasree@gmail.com>
2026-08-04 12:36:49 +0530

    day3changes

---
+++ track/c/number.java
@@ -0,0 +1,14 @@
+package track.c;
+public class number {
+    public static void main(String[] args){
+        int num=10;
+        if(num==0){
+            System.out.println("Number is Zero");
+        } else if(num > 0){
+            System.out.println("Number is Pastive");
+        }
+        else {
+            System.out.println("NUmber Negative");
+        }
+    }
+}
