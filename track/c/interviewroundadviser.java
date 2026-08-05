57d39818787d29f12a8037f7cd258a4c1479f49c
Mukkara Tejasree <mukkaratejasree@gmail.com>
2026-08-05 15:29:00 +0530

    day4changes

---
+++ track/c/interviewroundadviser.java
@@ -0,0 +1,46 @@
+package track.c;
+
+public class interviewroundadviser {
+    public class Main {
+    public static void main(String[] args) {
+
+        int roundCode = 4;
+        int attemptsUsed = 2;
+        int maxAttempts = 3;
+
+        String roundName;
+
+        System.out.print("Current Round: ");
+
+        switch (roundCode) {
+            case 1:
+                System.out.println("Aptitude Round");
+                break;
+
+            case 2:
+                System.out.println("Technical Round");
+                break;
+
+            case 3:
+                System.out.println("HR Round");
+                break;
+
+            case 4:
+            case 5:
+                System.out.println("Final Review");
+                break;
+
+            default:
+                System.out.println("Invalid Round");
+        }
+
+        String res = attemptsUsed < maxAttempts
+                ? "Attempt Available"
+                : "Attempt Limit Reached";
+
+        System.out.println("Attempt Status: " + res);
+    }
+}
+
+    
+}
