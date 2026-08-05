dc21f7cd8bd2d621459eb8bdec338f2f9b7b9b43
Mukkara Tejasree <mukkaratejasree@gmail.com>
2026-08-05 12:19:59 +0530

    day4changes

---
+++ track/c/pgm_for.java
@@ -0,0 +1,9 @@
+package track.c;
+
+public class pgm_for {
+    public static void main(String[] args){
+        for (int i= 1; i <= 5; i++ ) {
+            System.out.println(i);  
+        }
+    }
+}
