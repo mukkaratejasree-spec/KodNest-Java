bc2ac6c2c7067f1d7830111e7275c4e23c2ffee7
Mukkara Tejasree <mukkaratejasree@gmail.com>
2026-08-06 13:04:37 +0530

    day4changes

--- track/c/LabledLoopControlFlow.java
+++ track/c/LabledLoopControlFlow.java
@@ -5,6 +5,7 @@ public class LabledLoopControlFlow {
 
         outer:
         for(int i=1; i<=5; i++){
+            inner:
             for(int j=1; j<=5;j++){
                 if(j == 3){
                     break outer;
