01885cdc4e1cf3dfe2c8f932fe029d359cf8c3b5
Mukkara Tejasree <mukkaratejasree@gmail.com>
2026-08-06 12:27:35 +0530

    day4changes

---
+++ track/c/controlflow_break.java
@@ -0,0 +1,14 @@
+package track.c;
+
+public class controlflow_break {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                break;// stops the excecution and eneded the program or block
+            }
+            System.out.println(i);
+        }
+    }
+    
+}
---
+++ track/c/controlflow_continue.java
@@ -0,0 +1,13 @@
+package track.c;
+
+public class controlflow_continue {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                continue;// stops the current iteration and continues to the next iteration
+            }
+            System.out.println(i);
+        }
+    }
+    
+}
