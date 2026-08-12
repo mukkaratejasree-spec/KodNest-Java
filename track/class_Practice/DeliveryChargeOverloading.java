6db53911a3c027c142acda3e2379ee6909732aa0
Mukkara Tejasree <mukkaratejasree@gmail.com>
2026-08-12 15:45:00 +0530

    delivery

---
+++ track/class_Practice/DeliveryChargeOverloading.java
@@ -0,0 +1,33 @@
+package track.class_Practice;
+import java.util.Scanner;
+
+class DeliveryCalculator {
+    // One-parameter overload
+    int calculateCharge(int distance) {
+        return distance * 5;
+    }
+
+    // Two-parameter overload
+    int calculateCharge(int distance, int expressFee) {
+        return distance * 5 + expressFee;
+    }
+}
+
+public class DeliveryChargeOverloading {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int distance = scanner.nextInt();
+        int expressFee = scanner.nextInt();
+
+        DeliveryCalculator dc = new DeliveryCalculator();
+
+        // Call the one-parameter overload
+        System.out.println("Standard Charge: " + dc.calculateCharge(distance));
+
+        // Call the two-parameter overload
+        System.out.println("Express Charge: " + dc.calculateCharge(distance, expressFee));
+
+        scanner.close();
+    }
+}
