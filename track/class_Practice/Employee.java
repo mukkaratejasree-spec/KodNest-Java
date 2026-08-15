9048f248321e371281c8fd7a551f1cdfa6c48b4b
Mukkara Tejasree <mukkaratejasree@gmail.com>
2026-08-15 17:17:57 +0530

    employeedetails

---
+++ track/class_Practice/Employee.java
@@ -0,0 +1,37 @@
+package track.class_Practice;
+
+import java.util.Scanner;
+
+class Employee {
+    String name;
+    String role;
+
+    // No-argument constructor
+    Employee() {
+        role = "Trainee";
+    }
+
+    void displayProfile() {
+        System.out.println("Employee Name: " + name);
+        System.out.println("Employee Role: " + role);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Read employee name
+        String name = sc.nextLine();
+
+        // Create exactly one object
+        Employee e = new Employee();
+
+        // Store name
+        e.name = name;
+
+        // Display profile
+        e.displayProfile();
+        sc.close();
+    }
+}
