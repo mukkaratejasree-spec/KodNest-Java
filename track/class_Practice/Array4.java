ea4fcd8e378084560af6778309f3c0ab25179680
Mukkara Tejasree <mukkaratejasree@gmail.com>
2026-08-18 12:52:40 +0530

    array

---
+++ track/class_Practice/Array4.java
@@ -0,0 +1,27 @@
+package track.class_Practice;
+import java.util.Scanner;
+
+public class Array4 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int arr[] = new int[5];
+        int total = 0;
+
+        // Taking input
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Calculating total
+        for (int i = 0; i < arr.length; i++) {
+            total += arr[i];
+        }
+
+        // Printing total
+        System.out.println("Total: " + total);
+
+        sc.close();
+    }
+}
