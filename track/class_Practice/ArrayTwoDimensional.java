a5b9f9267a2e5a13c6a5f002fbc0d601bd4bbded
Mukkara Tejasree <mukkaratejasree@gmail.com>
2026-08-19 11:53:56 +0530

    2dArray

---
+++ track/class_Practice/ArrayTwoDimensional.java
@@ -0,0 +1,24 @@
+package track.class_Practice;
+import java.util.Scanner;
+public class ArrayTwoDimensional {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the elements ");
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int arr [][]= new int[r][c];
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Ouput: ");
+        for(int i = 0; i < r; i++){
+            for(int j = 0; j < c; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }   
+        sc.close();
+    }
+}
