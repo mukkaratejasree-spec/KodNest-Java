00b1afa31f938be2e4cd689d5444dfa9d4d6828a
Mukkara Tejasree <mukkaratejasree@gmail.com>
2026-08-19 12:18:46 +0530

    2dArray

---
+++ track/class_Practice/ArrayMatrix.java
@@ -0,0 +1,31 @@
+package track.class_Practice;
+import java.util.Scanner;
+
+public class ArrayMatrix {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+
+        int matrix[][] = new int[r][c];
+
+        // Reading matrix elements
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        // Printing matrix elements
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
