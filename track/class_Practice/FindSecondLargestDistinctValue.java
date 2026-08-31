4a6c4d4313508391ac634dfbe1e169aad4fd566c
Mukkara Tejasree <mukkaratejasree@gmail.com>
2026-08-31 15:30:14 +0530

    sollution

---
+++ track/class_Practice/FindSecondLargestDistinctValue.java
@@ -0,0 +1,32 @@
+package track.class_Practice;
+
+import java.util.Scanner;
+
+public class FindSecondLargestDistinctValue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = arr[0];
+        int secLargest = Integer.MIN_VALUE;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > largest) {
+                secLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secLargest && arr[i] != largest) {
+                secLargest = arr[i];
+            }
+        }
+
+        if (secLargest == Integer.MIN_VALUE)
+            System.out.println("No second largest distinct value");
+        else
+            System.out.println(secLargest);
+        sc.close();
+
+    }
+}
