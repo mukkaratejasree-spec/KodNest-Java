feac0dcdc8e0507f768eb8178e169b6451181465
Mukkara Tejasree <mukkaratejasree@gmail.com>
2026-09-07 17:27:39 +0530

    strings

---
+++ String/pgm5.java
@@ -0,0 +1,14 @@
+package String;
+
+public class pgm5 {
+    public static void main(Strings[] args) {
+        String s3 = new String("Java");
+        String s4 = new String("Java");
+        if (s3.equals(s4)) {
+            System.out.println("Strings are same");
+        } else {
+            System.out.println("Strings are not same");
+        }
+    }
+
+}
