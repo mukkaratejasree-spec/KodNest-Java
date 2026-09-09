7df35bd4c85907e2e4be15fd17156a9e454f758f
Mukkara Tejasree <mukkaratejasree@gmail.com>
2026-09-09 16:51:50 +0530

    strings

---
+++ String/StringMethod5.java
@@ -0,0 +1,15 @@
+package String;
+
+public class StringMethod5 {
+    public static void main(String[] args) {
+        String s1 = "Teja";
+        String s2 = "Poori";
+        String s3 = s1+ s2;
+        System.out.println(s3);
+        String t1 = new String("Anu");
+        String t2 = new String("Banu");
+        String res = t1.concat(t2);
+        System.out.println(res);
+    }
+    
+}
